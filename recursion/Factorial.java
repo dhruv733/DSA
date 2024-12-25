a9f256cd2598af54c8b693b471dd4baa60cfcad1
Dhruv Mahajan <106647123+dhruv733@users.noreply.github.com>
2024-12-25 15:31:22 +0530

    Update Factorial.java

--- recursion/Factorial.java
+++ recursion/Factorial.java
@@ -3,17 +3,12 @@ package recursion;
 public class Factorial {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println(factorial(5));
-
 	}
-	
 	static int factorial(int n) {
-		
 		if(n==1) {
 			return 1;
 		}
-		
 		return n* factorial(n-1);
 	}
 
